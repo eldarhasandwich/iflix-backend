305b7f3b35b68d5823c4acb0814d27dbb3f801e3
eldarhasandwich <hasanovicel@gmail.com>
2018-06-22 21:19:38 +1000

    created http entrypoints

--- index.js
+++ index.js
@@ -6,6 +6,22 @@ let config = require ('./config/development')
 
 const app = express()
 
+app.post('/rating', function (req, res) {
+    let userId = req.body.userId
+    let contentId = req.body.contentId
+    let rating = req.body.rating
+
+    console.log(userId + contentId + rating)
+})
+
+app.get('/rating/:contentId', function (req, res) {
+    console.log(req.params.contentId)
+    res.json({
+            contentId: req.params.contentId,
+            average: 3.2
+        })
+})
+
 app.listen(config.app.port, function (err) {
     if (err) {
         throw err
